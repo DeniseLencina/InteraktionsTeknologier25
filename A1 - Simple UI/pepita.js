95eb5b5cd5ee2b268b609aa43671213e9056509b
Denlenc <202407950@post.au.dk>
2025-09-07 23:53:33 +0200

    A1

--- A1 - Simple UI/pepita.js
+++ A1 - Simple UI/pepita.js
@@ -2,26 +2,27 @@ class Bird{
     constructor(){  /* this is a function */
         this.energy = 0;  /* this are the properties */
     }
-   /* eat(gramsOfSeeds){
+    eat(gramsOfSeeds){
         this.energy = this.energy + gramsOfSeeds * 4;
     }
     fly(kmFlying) {
         this.energy = this.energy - (10 + kmFlying *2);
     } 
-    Seconde example with no repetition of this. (dot operator)
-    */ 
-    eat(gramsOfSeeds){
+   // Seconde example with no repetition of this. (dot operator)
+    
+   /* eat(gramsOfSeeds){
         this.energy += gramsOfSeeds * 4; // add to the current value
     }
     fly(kmFlying) {
         this.energy -= (10 + kmFlying *2); // rest to the current value
     }
+    */
 }
 
 // Creating Pepita object
 let pepita = new Bird(); // Bird calls the Constructor function
-console.log("Pepita's starting energi is " + pepita.energy);
-pepita.eat(100)
+/* console.log("Pepita's starting energi is " + pepita.energy);
+pepita.eat(100) */
 console.log("The energi of Pepita after eating 100 is " + pepita.energy)
 
 function eat() {
